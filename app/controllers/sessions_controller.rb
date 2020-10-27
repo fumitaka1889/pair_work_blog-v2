0ae5156b503e2e7afa5191218cec4d202bb4648d
fimitaka shimizu <gilgala.aiglos@gmail.com>
2020-10-27 20:39:11 +0900

    ログインアプリ（ペアワーク）

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    binding.irb
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      flash.now[:danger] = '入力されたメールアドレスまたはパスワードはアカウントと一致しません。'
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to new_session_path
+  end
+end
